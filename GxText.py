6c9e2a7cae279121dfc9094e424a4f606d231cd1
Christopher Kyle Horton <christhehorton@gmail.com>
2014-02-22 22:18:09 -0500

    Fix copyright header.

--- GxText.py
+++ GxText.py
@@ -1,12 +1,12 @@
 # This file is part of GxText.
 # Copyright (C) 2014 Christopher Kyle Horton <christhehorton@gmail.com>
 
-# GxCalculator is free software: you can redistribute it and/or modify
+# GxText is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# GxCalculator is distributed in the hope that it will be useful,
+# GxText is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
